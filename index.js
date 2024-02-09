9c38862aa3c44e92ef673a6b2d548a7f9fbce785
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-10 00:16:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var is_function_1 = __importDefault(require("is-function"));
-function repeat(action, times, delay) {
-    if (delay === void 0) { delay = 1; }
-    if ((0, is_function_1.default)(action)) {
-        for (var i = 0; i < times; i++) {
-            setTimeout(action, i * delay);
-        }
+
+function repeat(callback, times, delay = 1) {
+    if (!(0, is_function_1.default)(callback)) {
+        throw new TypeError("Expected a function but got " + typeof callback + " instead.");
     }
-    else {
-        if ('string' !== typeof action)
-            throw new TypeError("Expected a \"string\" but got ".concat(typeof action, " instead."));
-        for (var i = 0; i < times; i++) {
-            setTimeout(function () { return eval(action); }, i * delay);
-        }
+
+    for (var i = 0; i < times; i++) {
+        setTimeout(function () {
+            callback(i);
+        }, i * delay);
     }
 }
+
 exports.default = repeat;
+
 //# sourceMappingURL=index.js.map
